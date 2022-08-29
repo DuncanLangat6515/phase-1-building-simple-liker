2f72d5640eda5e0ea737606e626d7344a243d388
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-08-29 16:53:34 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const liker = document.getElementsByClassName(like-glyph)
+liker.addEventListener('click', mimicServerCall);
 
 
 
